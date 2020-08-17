43f14825044f2d4f956dc31ad7d48592e281d9c0
Elie Housseini <eliashousseini@gmail.com>
2020-08-17 15:26:39 +0300

    set up redux

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,7 +6,9 @@
  */
 
 export function getLetterMatchCount(guessWord, secretWord) {
-  const secretLetterSet = new Set(secretWord.split(''))
-  const guessedLetterSet = new Set(guessWord.split(''))
+  const secretLetterSet = new Set(secretWord.split('')) // p a r t y
+  const guessedLetterSet = new Set(guessWord.split('')) // t r a i n
+
+  // returns the length of the array that contains the common letters
   return [...secretLetterSet].filter(letter => guessedLetterSet.has(letter)).length
 }
